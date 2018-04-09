e5f333eb2a450405c2dd5ca6439323a604319ac5
Lily <731124942@qq.com>
2018-04-09 17:52:20 +0800

    2018.4.9 完善注册过程

--- src/main/java/Dao/UserDao.java
+++ src/main/java/Dao/UserDao.java
@@ -1,26 +1,38 @@
 package Dao;
 
 import entity.User;
+import exception.DaoException;
+import exception.DataAccessException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public class UserDao {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public void save(User u){
-        /*System.out.println("执行查找DAO");
+    String sql = "INSERT INTO user(name,userName,passwork,address,mobilePhone) VALUE (? , ? , ? , ? , ?)";
+
+    public int save(User u) throws DaoException {
+        try {
+            int i = jdbcTemplate.update(sql, u.getName(), u.getUserName(), u.getPassword(), u.getAddress(), u.getPhoneNum());
+            if (i != 0) {
+                System.out.println("返回的数值" + i);
+            }
+            return i;
+        }catch (DataAccessException dae){
+            System.out.println("保存失败");
+            throw new DaoException( "保存失败" , dae );
+        }
+    }
+
+    public void find(User u){
+        System.out.println("执行查找DAO");
         String query = "SELECT id FROM user";
         Integer id  = jdbcTemplate.queryForObject(query,Integer.class);
-        System.out.println(id);*/
-
-        String sql="INSERT INTO user(name,userName,passwork,address,mobilePhone) VALUE (? , ? , ? , ? , ?)";
-       int i =  jdbcTemplate.update(sql , u.getName(),u.getUserName() , u.getPassword() , u.getAddress() , u.getPhoneNum());
-        System.out.println(i);
+        System.out.println("返回的数值"+id);
     }
 
     public JdbcTemplate getJdbcTemplate() {
--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -10,14 +10,18 @@ import util.StringHelper;
 public class UserService {
     private UserDao userdao;
 
-    public void save( User u){
+    public boolean save( User u){
         System.out.println("准备到dao");
 
         String password = u.getPassword() ; // 获取明文密码
         String encryptPassword = StringHelper.encrypt( password , MessageDigestType.MD5, null ); // 使用 MD5 加密算法对密码加密
         u.setPassword( encryptPassword ); // 将加密后的密码设置到 c 对象中
 
-        userdao.save( u ); // 将 c 对象传递给 save 方法，完成保存操作
+        int i =  userdao.save( u ); // 将 c 对象传递给 save 方法，完成保存操作
+        if(i == 1){
+        return  true;
+        }else
+            return false;
     }
 
     public UserDao getUserdao() {
